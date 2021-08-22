2af583d5ba30a0976c31488ba68a7ff76f2f922e
“Joseph <jtconnors94@gmail.com>
2021-08-22 14:55:53 -0400

    fixed admin models site, account pathing error, css static connect

--- django_duplex/settings.py
+++ django_duplex/settings.py
@@ -130,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -140,5 +144,5 @@ AUTH_USER_MODEL = "flashcard.User"
 ACCOUNT_ACTIVATION_DAYS = 7
 SIMPLE_BACKEND_REDIRECT_URL = '/'
 REGISTRATION_AUTO_LOGIN = True
-# LOGIN_REDIRECT_URL = ''
-# LOGOUT_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
