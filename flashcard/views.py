f7b21375a7d8b127cfe4da8a543ad266f8bcec2b
“Joseph <jtconnors94@gmail.com>
2021-08-25 12:05:02 -0400

    create card pathing

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import User, Flashcard, Deck
 from .forms import DeckForm, CardForm
+
 
 # Create your views here.
 def index(request):
     return render(request, "flashcard/index.html")
 
+@login_required
 def list_all_decks(request):
     decks = Deck.objects.all().order_by("title")
     return render(request, "flashcard/list_all_decks.html", {"decks": decks})
@@ -25,20 +28,19 @@ def create_deck(request):
             deck = form.save(commit=False)
             deck.user = request.user
             deck.save()
-            return redirect("view_deck", pk=deck.pk)
+            return redirect("create_card", pk=deck.pk)
     else:
         form = DeckForm()
     body = {"form": form}
     return render(request, "flashcard/create_deck.html", body)
 
-def create_card(request):
+def create_card(request, pk):
     if request.method == "POST":
         form = CardForm(data=request.POST)
         if form.is_valid():
             card = form.save(commit=False)
-            card.user = request.user
             card.save()
-            return redirect("view_deck", pk=card.pk)
+            return redirect("view_deck", pk=pk)
     else:
         form = CardForm()
     body = {"form": form}
